e85986a6f1223678c7e740ee290faa907181c1fe
limhangyeol <hang19663@gmail.com>
2023-10-02 16:15:55 +0900

    Initial project

---
+++ url-shortener/src/test/kotlin/com/tommy/urlshortener/UrlShortenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tommy.urlshortener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShortenerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
