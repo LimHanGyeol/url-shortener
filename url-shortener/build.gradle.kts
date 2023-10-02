e07b6ea0b4d0300a3c6de366dc7020fea1beac71
limhangyeol <hang19663@gmail.com>
2023-10-02 16:34:20 +0900

    Initial gradle

--- url-shortener/build.gradle.kts
+++ url-shortener/build.gradle.kts
@@ -20,8 +20,12 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
