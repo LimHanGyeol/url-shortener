a59073b72c7629590d55a83cb3bc46f9fad2a3b0
limhangyeol <hang19663@gmail.com>
2023-10-03 15:51:12 +0900

    ShortenUrl Entity 구현
    - ShortenUrl Entity 구현
    - BaseEntity 구현

---
+++ src/main/kotlin/com/tommy/urlshortener/config/UrlShortenerConfig.kt
@@ -0,0 +1,8 @@
+package com.tommy.urlshortener.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@Configuration
+@EnableJpaAuditing
+class UrlShortenerConfig
