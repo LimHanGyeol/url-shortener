383a52bcd8ff131048dec4643057028921df6946
Tommy <50355263+LimHanGyeol@users.noreply.github.com>
2023-10-03 15:54:14 +0900

    ShortenUrl Entity 구현 (#5)
    - ShortenUrl Entity 구현
    - BaseEntity 구현

---
+++ src/main/kotlin/com/tommy/urlshortener/domain/ShortenUrl.kt
@@ -0,0 +1,44 @@
+package com.tommy.urlshortener.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Lob
+import jakarta.persistence.Table
+
+@Entity
+@Table(name = "shorten_url")
+class ShortenUrl(
+
+    val shortenKey: Long,
+
+    @Lob
+    @Column(columnDefinition = "TEXT")
+    val originUrl: String,
+
+    val shortUrl: String,
+) : BaseEntity() {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ShortenUrl
+
+        return id == other.id
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+    override fun toString(): String {
+        return "ShortenUrl(shortenKey=$shortenKey, originUrl='$originUrl', shortUrl='$shortUrl', id=$id)"
+    }
+}
