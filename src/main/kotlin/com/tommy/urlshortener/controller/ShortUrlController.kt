cdb0ffe35f152a3cd7082f447a21c8dbc1856f7a
limhangyeol <hang19663@gmail.com>
2023-10-08 00:33:34 +0900

    ShortUrlController 개선
    - ShortUrlController에 URL 단축 기능 맵핑
    - TestCode 개선
    
    work items: #9

--- src/main/kotlin/com/tommy/urlshortener/controller/ShortUrlController.kt
+++ src/main/kotlin/com/tommy/urlshortener/controller/ShortUrlController.kt
@@ -2,16 +2,19 @@ package com.tommy.urlshortener.controller
 
 import com.tommy.urlshortener.dto.ShortUrlRequest
 import com.tommy.urlshortener.dto.ShortUrlResponse
+import com.tommy.urlshortener.service.UrlShortService
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class ShortUrlController {
+class ShortUrlController(
+    private val urlShortService: UrlShortService,
+) {
 
     @PostMapping("/shorten")
     fun shortUrl(@RequestBody @Validated shortUrlRequest: ShortUrlRequest): ShortUrlResponse {
-        return ShortUrlResponse("")
+        return urlShortService.shorten(shortUrlRequest)
     }
 }
