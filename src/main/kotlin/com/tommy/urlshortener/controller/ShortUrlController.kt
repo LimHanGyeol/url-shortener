0485a14ff7af540505bbb0a3a165b6d96abb4a73
limhangyeol <hang19663@gmail.com>
2023-10-09 00:03:38 +0900

    Url Redirection API 구현
    - 입력받은 단축 URL을 원본 URL로 Redirection 하는 API를 구현한다.
    
    work items: #18

--- src/main/kotlin/com/tommy/urlshortener/controller/ShortUrlController.kt
+++ src/main/kotlin/com/tommy/urlshortener/controller/ShortUrlController.kt
@@ -1,10 +1,13 @@
 package com.tommy.urlshortener.controller
 
+import com.tommy.urlshortener.dto.RedirectResponseEntity
 import com.tommy.urlshortener.dto.ShortUrlRequest
 import com.tommy.urlshortener.dto.ShortUrlResponse
 import com.tommy.urlshortener.service.UrlShortService
 import com.tommy.urlshortener.service.UrlValidator
 import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -19,5 +22,10 @@ class ShortUrlController(
     fun shortUrl(@RequestBody @Validated shortUrlRequest: ShortUrlRequest): ShortUrlResponse {
         urlValidator.validate(shortUrlRequest.originUrl)
         return urlShortService.shorten(shortUrlRequest)
+    }
+
+    @GetMapping("/{shortUrl}")
+    fun redirect(@PathVariable shortUrl: String): RedirectResponseEntity {
+        return RedirectResponseEntity("EysI9lHD")
     }
 }
