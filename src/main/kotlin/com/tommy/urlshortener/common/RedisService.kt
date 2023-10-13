12c62b636b758fa051a122774fc93b8bfcae45f6
limhangyeol <hang19663@gmail.com>
2023-10-13 23:59:31 +0900

    RedisService 기능 개선
    - Redis Key 생성 추가
    - objectMapper로 값을 관리하도록 설정
    - TypeReference를 외부에서 주입 받도록 변경해야함
    - StringRedisTemplate을 이용하여 별도의 Connection, RedisTemplate을 설정하지 않도록 한다.
    
    work items: #24

--- src/main/kotlin/com/tommy/urlshortener/common/RedisService.kt
+++ src/main/kotlin/com/tommy/urlshortener/common/RedisService.kt
@@ -1,43 +1,57 @@
 package com.tommy.urlshortener.common
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.data.redis.core.RedisTemplate
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.data.redis.core.ValueOperations
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.stereotype.Service
 import java.util.concurrent.TimeUnit
 
 @Service
 class RedisService(
-    private val redisTemplate: RedisTemplate<String, Any>,
+    @Value("\${spring.application.name:url-shortener}")
+    private val appName: String,
+    private val objectMapper: ObjectMapper,
+    private val redisTemplate: StringRedisTemplate,
 ) {
-    private val valueOperations: ValueOperations<String, Any> = redisTemplate.opsForValue()
+    private val valueOperations: ValueOperations<String, String> = redisTemplate.opsForValue()
 
-    fun set(key: String, value: Any, duration: Long, timeUnit: TimeUnit) {
-        redisTemplate.valueSerializer = Jackson2JsonRedisSerializer(value::class.java)
-        redisTemplate.opsForValue().set(key, value, duration, timeUnit)
+    fun set(key: String, value: Any, ttl: Long, timeUnit: TimeUnit) {
+        val redisKey = generateKey(key)
+        val serializedValue = objectMapper.writeValueAsString(value)
+        valueOperations.set(redisKey, serializedValue, ttl, timeUnit)
     }
 
-    fun get(key: String): Any? = redisTemplate.opsForValue().get(key)
+    fun <T> get(key: String): T? {
+        val redisKey = generateKey(key)
+        val value = valueOperations.get(redisKey)
+
+        return value?.let {
+            objectMapper.readValue(it, object : TypeReference<T>() {})
+        }
+    }
 
     fun increment(key: String, delta: Long = 1L): Int {
-        val incrementedValue = valueOperations.increment(key, delta)
+        val redisKey = generateKey(key)
+        val incrementedValue = valueOperations.increment(redisKey, delta)
         return incrementedValue?.toInt() ?: 1
     }
 
-    fun increment(key: String, delta: Long = 1L, ttl: Long, timeUnit: TimeUnit): Int {
-        val incrementedValue = valueOperations.increment(key, delta)
-        if (incrementedValue == delta) {
-            redisTemplate.expire(key, ttl, timeUnit)
-        }
-        return incrementedValue?.toInt() ?: 1
-    }
+    fun delete(key: String): Boolean = redisTemplate.delete(generateKey(key))
 
-    fun delete(key: String): Boolean = redisTemplate.delete(key)
-
-    fun hasKey(key: String): Boolean = redisTemplate.hasKey(key)
+    fun hasKey(key: String): Boolean = redisTemplate.hasKey(generateKey(key))
 
     fun deleteAll(key: String) {
-        val keys = redisTemplate.keys(key)
-        redisTemplate.delete(keys)
+        val redisKey = generateKey(key)
+        val keys = redisTemplate.keys("$redisKey:*")
+        if (keys.isNotEmpty()) {
+            redisTemplate.delete(keys)
+        }
+    }
+
+    private fun generateKey(key: String): String {
+        return "$appName:$key"
     }
 }
