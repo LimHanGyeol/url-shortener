ddfdba5f715d138b225e206ca48dd42140c33a26
limhangyeol <hang19663@gmail.com>
2023-10-19 23:35:30 +0900

    NotFoundException 추가
    work items: #23

---
+++ src/main/kotlin/com/tommy/urlshortener/exception/NotFoundException.kt
@@ -0,0 +1,5 @@
+package com.tommy.urlshortener.exception
+
+import org.springframework.http.HttpStatus
+
+class NotFoundException(pair: Pair<String, String>, vararg args: Any?) : BaseException(HttpStatus.NOT_FOUND.value(), pair.first, pair.second)
