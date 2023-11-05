b7d3a1ade09e61d4a07e26d7b530ec4fa1b0e43f
limhangyeol <hang19663@gmail.com>
2023-11-05 22:52:26 +0900

    TestMockConfig 개선
    - Solitray Test를 위해 외부 의존성 Mock 개선
    - redisTempalte의 경우 production code는 RedisTemplate<String, String>을 사용하고 있고, data redis의 기본 구현체를 이용하고 있다. 필요가 없으므로 제거한다.
    - redissonClient와 distributedAop의 경우 Solitray Test 기준 Redis Connection 맺을 필요가 없다. 정상 동작을 위해 해당 MockBean을 생성한다.
    - redisson의 경우 통합 테스트를 위해 TestContainer를 구축하도록 한다.
    
    work items: #30

--- src/test/kotlin/com/tommy/urlshortener/config/TestMockConfig.kt
+++ src/test/kotlin/com/tommy/urlshortener/config/TestMockConfig.kt
@@ -1,11 +1,12 @@
 package com.tommy.urlshortener.config
 
 import com.ninjasquad.springmockk.MockkBean
+import com.tommy.urlshortener.lock.DistributedLockAop
+import io.mockk.impl.annotations.InjectMockKs
+import org.redisson.api.RedissonClient
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.context.annotation.Bean
 import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory
 import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.core.RedisTemplate
 
 @TestConfiguration
 class TestMockConfig {
@@ -14,12 +15,13 @@ class TestMockConfig {
     class RedisMockConfig(
         @MockkBean private val redisConnectionFactory: RedisConnectionFactory,
         @MockkBean private val reactiveRedisConnectionFactory: ReactiveRedisConnectionFactory,
+    )
+
+    @TestConfiguration
+    class LockMockConfig(
+        @MockkBean private val redissonClient: RedissonClient
     ) {
-        @Bean
-        fun redisTemplate(): RedisTemplate<String, Any> {
-            val redisTemplate = RedisTemplate<String, Any>()
-            redisTemplate.connectionFactory = redisConnectionFactory
-            return redisTemplate
-        }
+        @InjectMockKs
+        private lateinit var distributedLockAop: DistributedLockAop
     }
 }
