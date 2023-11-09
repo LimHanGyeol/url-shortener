d5bca15f8b7545d29910d0fc59cf7acecaa9d314
limhangyeol <hang19663@gmail.com>
2023-11-09 10:30:18 +0900

    Exception 코드 개선
    - args 누락한 코드 추가
    - args 초기화 코드 작성하여 파라미터를 넣지 않아도 동작하도록 개선
    
    work items: #53

--- src/main/kotlin/com/tommy/urlshortener/exception/ServiceUnavailableException.kt
+++ src/main/kotlin/com/tommy/urlshortener/exception/ServiceUnavailableException.kt
@@ -2,4 +2,4 @@ package com.tommy.urlshortener.exception
 
 import org.springframework.http.HttpStatus
 
-class ServiceUnavailableException(pair: Pair<String, String>, vararg args: Any?) : BaseException(HttpStatus.SERVICE_UNAVAILABLE.value(), pair.first, pair.second)
+class ServiceUnavailableException(pair: Pair<String, String>, vararg args: Any?) : BaseException(HttpStatus.SERVICE_UNAVAILABLE.value(), pair.first, pair.second, args)
